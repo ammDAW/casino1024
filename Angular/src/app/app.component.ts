48291e876f49e565f3f9db129b662a7ac995e11a
Alberto Martinez <albertom.martinez@hotmail.com>
2021-04-06 14:03:46 +0200

    funcion PuntosUser para prueba

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular';
   logueado = false;
+  idUser = 1;
+  puntos = 0;
+  userName = "";
 
   getLogeado(logeado : boolean){
     this.logueado = logeado;
+  }
+
+  getPuntosUser(){
+    axios
+    .get('http://localhost:1337/points/'+this.idUser)
+    .then(response => {
+      // Handle success.
+      this.puntos = response.data.puntos;
+      this.userName = response.data.id_user.username;
+      
+   })
+    .catch(error => {
+      // Handle error.
+    
+      console.log('An error occurred:', error.response);
+    });
   }
 }
