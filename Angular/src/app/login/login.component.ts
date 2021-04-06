aa16139452aed2d0bb901f6e1d7e794af18d76ec
Alberto Martinez <albertom.martinez@hotmail.com>
2021-04-06 19:24:54 +0200

    funciones sacar id points y descripciones

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,47 +1,95 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import axios from 'axios';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
-  @Output() logueado = new EventEmitter();
-  jwtUser : any ;
-  idUser : any;
-  usernameUser : any;
+  
+  @Output() logueado = new EventEmitter(); //booleano emitido para hacer aparecer el login o la informacion del usuario
+  jwtUser : any ; //clave autentificacion usuario
+  idUser : any; //id usuario en tabla 'users'
+  usernameUser : any; //nick usuario
+  idPoints : any; //id tabla 'points' ligado al usuario
+  array :any
+  
   constructor() { }
 
   ngOnInit(): void {
-    
   }
 
+  //funciones creadas
   enviarLogin(logueado : boolean){
     this.logueado.emit(logueado);
   }
 
   buscarUsuario(user:string, pass:string){
-  axios
+    axios
     .post('http://localhost:1337/auth/local', {
-    identifier: user,
-    password: pass,
-  })
-  .then(response => {
-    // Handle success.
-    this.enviarLogin(true);
-    this.jwtUser = response.data.jwt;
-    this.idUser = response.data.user.id;
-    this.usernameUser = response.data.user.username;
-
-    console.log('Well done!');
-    console.log('User profile', response.data.user);
-    console.log('User token', response.data.jwt);
-    console.log(this.usernameUser);
-  })
-  .catch(error => {
-    // Handle error.
-    this.enviarLogin(false);
-    console.log('An error occurred:', error.response);
-  });
+      identifier: user,
+      password: pass,
+    })
+    .then(response => {
+      this.enviarLogin(true);
+      this.jwtUser = response.data.jwt;
+      this.idUser = response.data.user.id;
+      this.usernameUser = response.data.user.username;
+      
+      this.buscarPuntos();
+      this.idPoints = this.comparar(this.idUser);
+    })
+    .catch(error => {
+      this.enviarLogin(false);
+      console.log('An error occurred:', error.response);
+    });    
   }
+
+  buscarPuntos(){
+    axios
+    .get('http://localhost:1337/points')
+    .then(response => {
+      // Handle success.
+      function comparar ( a:any, b:any ){ return b.puntos - a.puntos; }
+      this.array = response.data;
+   })
+    .catch(error => {
+      // Handle error.
+      console.log('An error occurred:', error.response);
+    });  
+  }
+
+  comparar(idUser:any){
+    this.array.forEach(function(element:any){
+      //console.log(element)
+      if (element.id_user.id === idUser){
+        return element.id;
+      }
+      
+    });  
+  }
+  /*buscarPuntos(idUser:number){
+    axios
+    .get('http://localhost:1337/points')
+    .then(response => {
+      response.data.forEach(function(element:any){
+        if (element.id_user.id = idUser){
+          console.log(element.id_user.id)
+          return;
+        }
+      for(let element of response.data){
+        if (element.id_user.id = idUser){
+          //console.log(element.id_user.id);
+         console.log(element.id);
+        }
+      }
+   })
+    .catch(error => {
+      // Handle error.
+      console.log('An error occurred:', error.response);
+    });
+  }*/
+
 }
