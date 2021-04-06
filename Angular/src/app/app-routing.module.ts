60d85ca8c418a2ffb23c0641d19cf2b9deed5802
Alberto Martinez <albertom.martinez@hotmail.com>
2021-04-06 14:03:21 +0200

    ruta bingo creada

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BingoComponent } from './bingo/bingo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bingo', component: BingoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
