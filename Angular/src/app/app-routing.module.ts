c841d6e4ebcf0c13ce9a19c45d619a375a53fb58
Alberto Martinez <albertom.martinez@hotmail.com>
2021-04-20 19:54:30 +0200

    add rutas

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BingoComponent } from './bingo/bingo.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'bingo', component: BingoComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'bingo', component: BingoComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
